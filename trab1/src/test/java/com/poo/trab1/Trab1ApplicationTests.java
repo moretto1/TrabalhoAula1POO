2b6a9325f6b5966f0d7494d58457704e4bd80baf
moretto1 <bmoretto404@gmail.com>
2021-08-05 21:21:35 -0300

    Initial commit

---
+++ trab1/src/test/java/com/poo/trab1/Trab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poo.trab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
